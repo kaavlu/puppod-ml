22b22ca69fa77a94ada9302a8f93f2ec21566d79
Manav Khanvilkar <kaavlu@gmail.com>
2024-07-21 14:16:15 -0700

    GIT LFS was not working so split into 2 files

--- gs_json.py
+++ gs_json.py
@@ -2,8 +2,8 @@ import csv
 import json
 from collections import defaultdict
 
-# Function to convert CSV to JSON
-def csv_to_json(csv_file_path, json_file_path):
+# Function to convert CSV to JSON and split data into two files
+def csv_to_json(csv_file_path, json_file_path1, json_file_path2):
     # Dictionary to store the final data
     data = defaultdict(lambda: defaultdict(list))
     
@@ -22,12 +22,23 @@ def csv_to_json(csv_file_path, json_file_path):
     # Convert defaultdict to a regular dict for JSON serialization
     data = {dog_id: dict(sessions) for dog_id, sessions in data.items()}
     
-    with open(json_file_path, mode='w') as json_file:
-        json.dump(data, json_file, indent=4)
+    # Split the data into two parts
+    items = list(data.items())
+    mid_index = len(items) // 2
+    
+    data_part1 = dict(items[:mid_index])
+    data_part2 = dict(items[mid_index:])
+    
+    with open(json_file_path1, mode='w') as json_file1:
+        json.dump(data_part1, json_file1, indent=4)
+    
+    with open(json_file_path2, mode='w') as json_file2:
+        json.dump(data_part2, json_file2, indent=4)
 
-# Define your CSV file path and JSON file path
+# Define your CSV file path and JSON file paths
 csv_file_path = 'data/filtered/GameSessions_filtered.csv'
-json_file_path = 'test.json'
+json_file_path1 = 'total_data1.json'
+json_file_path2 = 'total_data2.json'
 
-# Call the function to convert CSV to JSON
-csv_to_json(csv_file_path, json_file_path)
+# Call the function to convert CSV to JSON and split the data
+csv_to_json(csv_file_path, json_file_path1, json_file_path2)
